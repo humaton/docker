moby/moby

Replace 'commit --run' with 'commit --change'
```docker commit --change``` allows specifying standard changes to be applied to the
new image. It deprecates the less user-friendly and less stable *commit --run*.

Changes are expressed in the Dockerfile syntax, and can be used  to modify the image
metadata. For example:

```docker commit --change 'WORKDIR /home/bob'``` will commit the image
with */home/bob* as the default working directory.

```docker commit --change 'EXPOSE 80 53/udp'``` will commit the image
with network ports *80/tcp* and *53/udp* marked as exposed.

```docker commit --change 'VOLUME /var/data'``` will commit the image
with */var/data* marked as a volume.

The following Dockerfile instructions can be used as a change:

* ENTRYPOINT
* CMD
* USER
* WORKDIR
* ENV
* VOLUME
* EXPOSE
* ONBUILD

The syntax for these commands is identical in *--change* and in a
Dockerfile. See the Dockerfile reference for a complete syntax reference.

Note that some fields which could be changed directly with the
deprecated ```--run``` can no longer be changed with the new
```--change```. This is by design, for 2 reasons:

1) Not every field in the underlying Config object should be
automatically exposed for modification. This gives us a stable
indirection to control the exact set of allowed modifications.

2) If the Dockerfile syntax does not allow a legitimate modification,
we can simply enrich the syntax, fixing the problem in a unified way
both in Dockerfile and *docker commit*.

Docker-DCO-1.1-Signed-off-by: Solomon Hykes <solomon@docker.com> (github: shykes)

api/server/server.go
@@ -28,6 +28,7 @@ import (
 	"github.com/dotcloud/docker/pkg/user"
 	"github.com/dotcloud/docker/pkg/version"
 	"github.com/dotcloud/docker/registry"
+	"github.com/dotcloud/docker/runconfig"
 	"github.com/dotcloud/docker/utils"
 	"github.com/gorilla/mux"
 )
@@ -349,19 +350,20 @@ func postCommit(eng *engine.Engine, version version.Version, w http.ResponseWrit
 		return err
 	}
 	var (
-		config engine.Env
-		env    engine.Env
-		job    = eng.Job("commit", r.Form.Get("container"))
+		legacyconfig runconfig.Config
+		env          engine.Env
+		job          = eng.Job("commit", r.Form.Get("container"))
 	)
-	if err := config.Decode(r.Body); err != nil {
-		utils.Errorf("%s", err)
-	}
-
 	job.Setenv("repo", r.Form.Get("repo"))
 	job.Setenv("tag", r.Form.Get("tag"))
 	job.Setenv("author", r.Form.Get("author"))
 	job.Setenv("comment", r.Form.Get("comment"))
-	job.SetenvSubEnv("config", &config)
+	job.Setenv("changes", r.Form.Get("changes"))
+	// Prepend fields from legacy config object, for reverse compatibility.
+	if err := json.NewDecoder(r.Body).Decode(&legacyconfig); err != nil {
+		return err
+	}
+	job.Setenv("changes", legacyconfig.AsScript()+"\n"+job.Getenv("changes"))
 
 	var id string
 	job.Stdout.AddString(&id)

api/server/server_unit_test.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"github.com/dotcloud/docker/api"
 	"github.com/dotcloud/docker/engine"
+	"github.com/dotcloud/docker/runconfig"
 	"github.com/dotcloud/docker/utils"
 	"io"
 	"net/http"
@@ -13,6 +14,34 @@ import (
 	"os"
 	"testing"
 )
+
+func TestPostCommit(t *testing.T) {
+	eng := tmpEngine(t)
+	defer rmEngine(eng)
+	var called bool
+	eng.Register("commit", func(job *engine.Job) engine.Status {
+		if job.Getenv("changes") != "user solomon\nentrypoint [\"awesome\",\"command\"]\nenv foo bar\nentrypoint echo" {
+			t.Fatalf("%#v\n", job.Getenv("changes"))
+		}
+		if job.Getenv("repo") != "myimage" {
+			t.Fatalf("%#v\n", job.Getenv("repo"))
+		}
+		if job.Getenv("tag") != "mytag" {
+			t.Fatalf("%#v\n", job.Getenv("tag"))
+		}
+		called = true
+		return engine.StatusOK
+	})
+	// legacy config object
+	cfg := &runconfig.Config{}
+	cfg.User = "solomon"
+	cfg.Entrypoint = []string{"awesome", "command"}
+	r := serveRequest("POST", "/commit?repo=myimage&tag=mytag&changes=env foo bar\nentrypoint echo", toJson(cfg, t), eng, t)
+	if !called {
+		t.Fatalf("handler was not called")
+	}
+	readEnv(r.Body, t)
+}
 
 func TestGetBoolParam(t *testing.T) {
 	if ret, err := getBoolParam("true"); err != nil || !ret {

humaton/docker

Conflicts:
	api/client/commands.go
	api/server/server.go
	api/server/server_unit_test.go
	integration/api_test.go
	server/buildfile.go
	server/server.go

api/server/server.go
@@ -32,6 +32,7 @@ import (
 	"github.com/docker/docker/pkg/systemd"
 	"github.com/docker/docker/pkg/version"
 	"github.com/docker/docker/registry"
+	"github.com/docker/docker/runconfig"
 	"github.com/docker/docker/utils"
 )
 
@@ -458,7 +459,7 @@ func postCommit(eng *engine.Engine, version version.Version, w http.ResponseWrit
 		return err
 	}
 	var (
-		config       engine.Env
+		legacyconfig runconfig.Config
 		env          engine.Env
 		job          = eng.Job("commit", r.Form.Get("container"))
 		stdoutBuffer = bytes.NewBuffer(nil)
@@ -468,9 +469,9 @@ func postCommit(eng *engine.Engine, version version.Version, w http.ResponseWrit
 		return err
 	}
 
-	if err := config.Decode(r.Body); err != nil {
-		log.Errorf("%s", err)
-	}
+	//if err := config.Decode(r.Body); err != nil {
+	//	log.Errorf("%s", err)
+	//}
 
 	if r.FormValue("pause") == "" && version.GreaterThanOrEqualTo("1.13") {
 		job.Setenv("pause", "1")
@@ -484,6 +485,11 @@ func postCommit(eng *engine.Engine, version version.Version, w http.ResponseWrit
 	job.Setenv("comment", r.Form.Get("comment"))
 	job.Setenv("changes", r.Form.Get("changes"))
 	job.SetenvSubEnv("config", &config)
+	// Prepend fields from legacy config object, for reverse compatibility.
+	if err := json.NewDecoder(r.Body).Decode(&legacyconfig); err != nil {
+		return err
+	}
+	job.Setenv("changes", legacyconfig.AsScript()+"\n"+job.Getenv("changes"))
 
 	job.Stdout.Add(stdoutBuffer)
 	if err := job.Run(); err != nil {

api/server/server_unit_test.go
@@ -4,6 +4,10 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"github.com/docker/docker/api"
+	"github.com/docker/docker/engine"
+	"github.com/docker/docker/runconfig"
+	"github.com/docker/docker/utils"
 	"io"
 	"net/http"
 	"net/http/httptest"
@@ -15,6 +19,34 @@ import (
 	"github.com/docker/docker/engine"
 	"github.com/docker/docker/pkg/version"
 )
+
+func TestPostCommit(t *testing.T) {
+	eng := tmpEngine(t)
+	defer rmEngine(eng)
+	var called bool
+	eng.Register("commit", func(job *engine.Job) engine.Status {
+		if job.Getenv("changes") != "user solomon\nentrypoint [\"awesome\",\"command\"]\nenv foo bar\nentrypoint echo" {
+			t.Fatalf("%#v\n", job.Getenv("changes"))
+		}
+		if job.Getenv("repo") != "myimage" {
+			t.Fatalf("%#v\n", job.Getenv("repo"))
+		}
+		if job.Getenv("tag") != "mytag" {
+			t.Fatalf("%#v\n", job.Getenv("tag"))
+		}
+		called = true
+		return engine.StatusOK
+	})
+	// legacy config object
+	cfg := &runconfig.Config{}
+	cfg.User = "solomon"
+	cfg.Entrypoint = []string{"awesome", "command"}
+	r := serveRequest("POST", "/commit?repo=myimage&tag=mytag&changes=env foo bar\nentrypoint echo", toJson(cfg, t), eng, t)
+	if !called {
+		t.Fatalf("handler was not called")
+	}
+	readEnv(r.Body, t)
+}
 
 func TestGetBoolParam(t *testing.T) {
 	if ret, err := getBoolParam("true"); err != nil || !ret {